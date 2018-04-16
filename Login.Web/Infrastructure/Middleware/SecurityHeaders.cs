766ab7fd2eb0c0fcb653102a8d1a802cc1770230
Henrik Binggl <henrik@binggl.net>
2018-04-16 22:05:53 +0200

    Change security-policy to allow inline svg in css

--- Login.Web/Infrastructure/Middleware/SecurityHeaders.cs
+++ Login.Web/Infrastructure/Middleware/SecurityHeaders.cs
@@ -19,7 +19,7 @@ namespace Login.Web.Infrastructure.Middleware
             { "Access-Control-Allow-Origin", "APP_BASE_URL"},
             { "Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload"},
             { "Referrer-Policy", "same-origin"},
-            { "Content-Security-Policy", "default-src APP_BASE_URL; script-src APP_BASE_URL 'unsafe-inline'; style-src APP_BASE_URL 'unsafe-inline'"},
+            { "Content-Security-Policy", "default-src APP_BASE_URL; script-src APP_BASE_URL 'unsafe-inline'; style-src APP_BASE_URL 'unsafe-inline'; img-src APP_BASE_URL 'self' data:"},
         };
 
         public SecurityHeaders(RequestDelegate next, SecurityHeaderOptions options)
